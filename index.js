907c1711ea239eaede05754a530fa15f2d164b18
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2024-11-12 03:30:16 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    const register = document.querySelector('.register');
+    menu.classList.toggle('show');
+    register.classList.toggle('show');
+}
+
